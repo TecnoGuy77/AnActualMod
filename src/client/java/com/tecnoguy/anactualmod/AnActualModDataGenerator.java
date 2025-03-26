7fa372f44c6b0c640d1c7b6990326f16031aebfc
TecnoGuy77 <138028703+tecnoguy0@users.noreply.github.com>
2025-03-26 10:10:27 +0530

    Initial commit for my project

---
+++ src/client/java/com/tecnoguy/anactualmod/AnActualModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tecnoguy.anactualmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnActualModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
